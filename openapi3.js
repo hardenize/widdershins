41979b2854821351cd7d3429e52b6f8fd1f22a16
Igor Spasić <igor.spasic@redsift.io>
2024-04-16 13:50:02 +0200

    Revert "Added code to sort endpoints inside categories"
    This reverts commit 93fa20366b699126381c892aff03fe686329db2c.

--- openapi3.js
+++ openapi3.js
@@ -30,8 +30,6 @@ function convertToToc(source,data) {
             resources[tag.name] = { count: 0, methods: {}, description: tag.description, externalDocs: tag.externalDocs };
         }
     }
-
-
     for (var p in source.paths) {
         for (var m in source.paths[p]) {
             if ((m !== 'parameters') && (m !== 'summary') && (m !== 'description') && (!m.startsWith('x-'))) {
@@ -59,19 +57,9 @@ function convertToToc(source,data) {
         }
     }
     for (let r in resources) {
-        if (resources[r].count <= 0){
-            delete resources[r];}
-        else{
-            //Sort fields in methods object asc
-            const sortedKeys = Object.keys(resources[r].methods).sort();
-            const sortedObj = sortedKeys.reduce((acc, key) => {
-                acc[key] = resources[r].methods[key];
-                return acc;
-            }, {});
-            resources[r].methods = sortedObj;
-        }
+        if (resources[r].count <= 0) delete resources[r];
     }
-    return resources
+    return resources;
 }
 
 function getTagGroup(tag, tagGroups){
